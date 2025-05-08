cf00db1f3c35bcc67e6c2901ed2a40e67fd7dee7
amandaasu <amanda34@asu.edu>
2025-05-07 17:25:14 -0700

    frontend pages navigation

---
+++ frontend/src/app/cart/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Cart() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Cart Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/app/product/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Product Page</h1>
+    </div>
+  );
+}
